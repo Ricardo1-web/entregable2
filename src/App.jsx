4a1925cba96e9d221a47225affd26e258b6b5be3
Ricardo Pulido Péreze <pzy_boy@hotmail.com>
2023-06-18 21:35:03 -0600

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 import Weather from './components/Weather'
 import Loader from './components/Loader'
 
+
+
 function App() {
-  const [weatherInfo, setWeatherInfo] = useState(null )
+  const [weatherInfo, setWeatherInfo] = useState(null)
+  const [isDark, setIsDark] = useState(false)
 
   const success = (pos) => {
     const lat = pos.coords.latitude
@@ -24,14 +26,32 @@ function App() {
     navigator.geolocation.getCurrentPosition(success)
   }, [])
 
+const handleToggleTheme = () => setIsDark(!isDark)
+
+  useEffect(() => {
+if(isDark){
+  document.documentElement.classList.add('dark')
+}else{
+  document.documentElement.classList.remove('dark')
+}
+  }, [isDark])
+
   return (
-    <main className='"text-center grid gap-0" fixed w-full  min-h-screen text-white justify-center items-center font-principal-font p-40'>
-            <div className='bg-white/40 w-70 p-2 rounded-md text-left '>
-                <input
-                    type="text"
-                    placeholder='Search for a city'
-                    className='  bg-transparent font-bold uppercase text-left items-center' />
-            </div>
+    <main className='" text-center grid gap-0" fixed w-full  min-h-screen text-white justify-center items-center font-principal-font p-40'>
+      
+      <button onClick={handleToggleTheme} className="mx-auto block mb-2 text-2xl hover:text-blue-900 transition-colors">
+        {
+          isDark ? <i className='bx bxs-sun' ></i> : <i className='bx bxs-moon' ></i>
+        }
+        
+      </button>
+
+      <div className='bg-white/40 w-70 p-2 rounded-md text-left '>
+        <input
+          type="text"
+          placeholder='Search for a city'
+          className='  bg-transparent font-bold uppercase text-left items-center' />
+      </div>
       {
         weatherInfo ? <Weather weatherInfo={weatherInfo} /> : <Loader />
       }
